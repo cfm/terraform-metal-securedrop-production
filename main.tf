3b14e82b06d031ddd8877866bc494bd562c7fde0
Cory Francis Myers <cfm@panix.com>
2021-09-28 11:46:32 -0700

    use cloud-init to mimic expected "Set Up the Servers" configuration

--- main.tf
+++ main.tf
@@ -22,6 +22,10 @@ data "metal_project" "project" {
   name = var.project
 }
 
+data "template_file" "user_data" {
+  template = file("user_data.yml")
+}
+
 resource "metal_device" "app" {
   hostname         = "sd-app"
   plan             = "t1.small.x86"
@@ -29,6 +33,7 @@ resource "metal_device" "app" {
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = data.metal_project.project.id
+  user_data        = data.template_file.user_data.rendered
 }
 
 resource "metal_device" "mon" {
@@ -38,4 +43,5 @@ resource "metal_device" "mon" {
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = data.metal_project.project.id
+  user_data        = data.template_file.user_data.rendered
 }
